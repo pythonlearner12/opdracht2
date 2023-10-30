414880d8c76186c6c8aac11d8a45cc5b8c8153a6
Fred <36563@ma-web.nl>
2023-10-30 10:03:42 +0100

    opdracht 6 gedaan

--- helloworld.js
+++ helloworld.js
@@ -1,23 +1,36 @@
+//
+//
+//
+//let x = 0;
+//
+//function recurse(){
+//
+//    console.log(x);
+//    x+=1
+//
+//    if(x >10){
+//        return;
+//    }
+//
+//
+//    recurse();
+//    
+//
+//
+//
+//}
+//
+//recurse();
 
-
-
-let x = 0;
-
-function recurse(){
-
-    console.log(x);
-    x+=1
-
-    if(x >10){
-        return;
+function som(){
+    let array = 0
+    for(let x = 1; x <= 100; x++){
+        array+=x
+        
     }
-
-
-    recurse();
-    
-
+    console.log(array)
 
 
 }
 
-recurse();
+som()
